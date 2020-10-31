5513a56dbcc17996214c102e8092f083f5d38a60
Stanisław Kaczmarek <stanislaw.kaczmarek@student.put.poznan.pl>
2020-10-31 17:47:30 +0100

    Removed vanilla leveldb

--- scripts/setvars.sh
+++ scripts/setvars.sh
@@ -8,9 +8,7 @@ export DRAMBUFFSZ=64
 #NVM buffer size in MB
 export NVMBUFFSZ=4096
 export INPUTXML=$NOVELSMSCRIPT/input.xml
-#Vanilla LevelDB benchmark
-export NOVELSM_VANILLA=$NOVELSMSRC/novelsm-1.20
-export DBBENCH_VANLILLA=$NOVELSM_VANILLA/out-static
+
 export PARA=40
 export NUMA_AFFINITY=0
 #Numa binding 
